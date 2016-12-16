1901d9d6d8c6a5bf47f2be0a2b68b2bef3d996a6
Eryk <eryk@helenowski.com>
2016-12-16 11:55:41 -0600

    change contents to strings

--- src/adventure/core.clj
+++ src/adventure/core.clj
@@ -10,31 +10,31 @@
 for a game or something."
            :title "in the foyer"
            :dir {:south :grue-pen, :north :Dough-House}
-           :contents #{:raw-egg}}
+           :contents #{"raw egg"}}
 
   :grue-pen {:desc "It is very dark.  You are about to be eaten by a grue."
               :title "in the grue pen"
               :dir {:north :foyer}
-              :contents #{:very-sharp-pencil}}
+              :contents #{"very sharp pencil"}}
 
   :Dough-House {:desc "It smells nice in here, almost like a pillsbury dough boy's armpit."
               :title "in the dough House"
               :dir {:down :Basement, :up :Attic, :east :Lounge , :north :Attic }
-              :contents #{:cinnabon, :knife }}
+              :contents #{"cinnabon", "knife" }}
 
 
   :Basement {:desc "It is dark and cold in here. You desperately need to pee, and want to leave."
               :title "in the dungeon"
               :dir {:east :Billiard-Room, :down :Dough-House, :up :Attic}
 
-              :contents #{:cobwebs, :a-rusty-screw}}
+              :contents #{"cobwebs", "a rusty screw"}}
 
 
 
  :Attic {:desc "It's very small and cramped up here... What's that smell?"
              :title "in the Attic"
              :dir {:east :Dining-Room, :west :Hall, :down :Dough-House, :up :Attic }
-             :contents #{:lamp, :key}}
+             :contents #{"lamp", "key"}}
 
 
  :Hall {:desc "A well lit hallway, there are many doors that lead to unknown locations, which route will you take?"
@@ -45,23 +45,23 @@ for a game or something."
  :Storage-Room {:desc "Lots of miscellaneous items in this room, it only has one other door besides the entrance though."
             :title "in the Storage-Room"
             :dir {:east :Hall , :up :Fun-House }
-            :contents #{:cork, :tweezers, :baseball, :phallic-object}}
+            :contents #{"cork", "tweezers", "baseball", "phallic object"}}
 
 
  :Dining-Room {:desc "The livestock seems to be running free in the Dining Room. A chicken decides to pounce on your face and begins scratching your eye balls. What would you like to do?"
              :title "in the Dining-Room"
              :dir {:east :Kitchen, :down :Dough-House, :up :Attic, :west :Ballroom}
-             :contents #{:plate}}
+             :contents #{"plate"}}
 
  :Ballroom {:desc "Soft music plays in the background. You sit down on the plush couch to rest... You are rudely awakened by a piano that seems to be barelling towards your face, what woudl you like to do?"
              :title "in the Ballroom"
              :dir {:east :Hall, :down :Storage-Room, :up :grue-pen}
-             :contents #{:vinyl-record}}
+             :contents #{"vinyl record"}}
 
  :Kitchen {:desc "More livestock running around here... the chef is wrestling with a rat in his hair and pots and pans seem to be flying about the room"
              :title "in the Kitchen"
              :dir {:west :Dining-Room, :north :Hall, :down :foyer}
-             :contents #{:turkey}}
+             :contents #{"turkey"}}
 
   :Billiard-Room {:desc "Does anyone really play this game seriously? An empty room with a pool table in the center...Balls and sticks are strewn all over, it eerily reminds you of home"
             :title "in the Billard Room"
@@ -78,7 +78,7 @@ for a game or something."
  :Guest-Room {:desc "Lesson Number 1, trust no one. A nice stranger invites you into the guest room. He lets you sleep there for the night"
             :title "in the Guest Room, sucker"
             :dir {:east :foyer , :south :foyer , :down :foyer , :up :foyer ,  :west :foyer , :north :foyer }
-            :contents #{:cyanide, :happiness}}
+            :contents #{"cyanide", "happiness"}}
 
 
  :Fun-House {:desc "At long last you have reached the end of the line. Two strange men wearing rainbow turbans wait with their arms crossed at the opposite end of the room. They look suspicious...but you approach them eager to exit the nightmare"
